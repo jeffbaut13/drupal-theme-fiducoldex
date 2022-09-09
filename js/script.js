d26e713cc3e534082b2867c9bce0020297a53fcd
jefersson bautista <98704711+jeffbaut13@users.noreply.github.com>
2022-09-09 18:09:03 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,1 @@
-$('.collapseTwo').draggable({
-  axis: 'x',
-  drag: function(event, ui) {
-    var left = ui.position.left,
-        offsetWidth = ($(this).width() - $(this).parent().width()) * -1;
 
-    if (left > 0) {
-      ui.position.left = 0;
-    }
-    if (offsetWidth > left) {
-      ui.position.left = offsetWidth;
-    }
-  }
-});
